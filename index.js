732fc942c0f72f7178b683bf35251d1add00c5cb
Omari99 <mariam.ali01@student.moringaschool.com>
2022-12-11 17:00:39 +0300

    comleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,112 @@
 // Your code here
+let dataEmployees = [
+    ["Thor", "Odinsson", "Electrical Engineer", 45],
+    ["Loki", "Laufeysson-Odinsson", "HR Representative", 35],
+    ["Natalia", "Romanov", "CEO", 150],
+    ["Darcey", "Lewis", "Intern", 15],
+    ["Jarvis", "Stark", "CIO", 125],
+    ["Anthony", "Stark", "Angel Investor", 300],
+    ["Byron", "Poodle", "Mascot", 3],
+    ["Julius", "Caesar", "General", 27],
+    ["Rafiki", "", "Aide", 10],
+    ["Simba", "", "King", 100]
+  ]
+
+  let timesIn = [
+    ["Thor", ["2018-01-01 0800", "2018-01-02 0800", "2018-01-03 0800"]],
+    ["Loki", ["2018-01-01 0700", "2018-01-02 0700", "2018-01-03 0600"]],
+    ["Natalia", ["2018-01-01 1700", "2018-01-02 1800", "2018-01-03 1300"]],
+    ["Darcey", ["2018-01-01 0700", "2018-01-02 0800", "2018-01-03 0800"]],
+    ["Jarvis", ["2018-01-01 0500", "2018-01-02 0500", "2018-01-03 0500"]],
+    ["Anthony", ["2018-01-01 1400", "2018-01-02 1400", "2018-01-03 1400"]]
+  ]
+  let csvTimesOut = [
+    ["Thor", ["2018-01-01 1600", "2018-01-02 1800", "2018-01-03 1800"]],
+    ["Loki", ["2018-01-01 1700", "2018-01-02 1800", "2018-01-03 1800"]],
+    ["Natalia", ["2018-01-01 2300", "2018-01-02 2300", "2018-01-03 2300"]],
+    ["Darcey", ["2018-01-01 1300", "2018-01-02 1300", "2018-01-03 1300"]],
+    ["Jarvis", ["2018-01-01 1800", "2018-01-02 1800", "2018-01-03 1800"]],
+    ["Anthony", ["2018-01-01 1600", "2018-01-02 1600", "2018-01-03 1600"]]
+  ]
+let createEmployeeRecord = function(row){
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = function(employeeRowData) {
+    return employeeRowData.map(function(row){
+        return createEmployeeRecord(row)
+    })
+}
+
+let createTimeInEvent = function(employee, stampedDate){
+    let [date, hour] = stampedDate .split(' ')
+
+    employee.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return employee
+}
+
+let createTimeOutEvent = function(employee, stampedDate){
+    let [date, hour] = stampedDate.split(' ')
+
+    employee.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return employee
+}
+
+let hoursWorkedOnDate = function(employee, seekDate){
+    let eventIn = employee.timeInEvents.find(function(e){
+        return e.date === seekDate
+    })
+
+    let eventOut = employee.timeOutEvents.find(function(e){
+        return e.date === seekDate
+    })
+
+    return (eventOut.hour - eventIn.hour) / 100
+}
+
+let wagesEarnedOnDate = function(employee, dateSeek){
+    let rawWages = hoursWorkedOnDate(employee, dateSeek)
+        * employee.payPerHour
+    return parseFloat(rawWages.toString())
+}
+
+let allWagesFor = function(employee){
+    let acceptableDates = employee.timeInEvents.map(function(e){
+        return e.date
+    })
+
+    let payable = acceptableDates.reduce(function(memo, d){
+        return memo + wagesEarnedOnDate(employee, d)
+    }, 0)
+
+    return payable
+}
+
+let findEmployeeByFirstName = function(srcArray, firstName) {
+  return srcArray.find(function(rec){
+    return rec.firstName === firstName
+  })
+}
+
+let calculatePayroll = function(arrayEmployeeRecord){
+    return arrayEmployeeRecord.reduce(function(memo, rec){
+        return memo + allWagesFor(rec)
+    }, 0)
+}
